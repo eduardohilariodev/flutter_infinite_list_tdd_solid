f98ab8277f48a36f49c0c0a174b5cbd934e00ff5
Eduardo Hilário <eduhilario@proton.me>
2023-10-10 16:49:03 -0300

    tests: mockdioadapter

--- test/core/network/http_service_dio_impl_test.dart
+++ test/core/network/http_service_dio_impl_test.dart
@@ -5,37 +5,42 @@ import 'package:flutter_infinite_list_tdd_solid/core/error/exceptions.dart';
 import 'package:flutter_infinite_list_tdd_solid/core/network/http_service.dart';
 import 'package:flutter_infinite_list_tdd_solid/core/network/http_service_dio_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../fixtures/fixture_reader.dart';
 
 class MockDio extends Mock implements Dio {}
 
+class MockDioAdapter extends Mock implements DioAdapter {}
+
 void main() {
-  late MockDio mockDio;
+  late Dio mockDio;
   late HttpServiceDioImpl httpServiceDioImpl;
-  setUp(() async {
-    mockDio = MockDio();
-    httpServiceDioImpl = HttpServiceDioImpl(mockDio);
-  });
+  late DioAdapter mockDioAdapter;
+
   final mockData = json.decode(fixture('post.json')) as Map<String, dynamic>;
   final mockResponse = HttpResponse(mockData, 200);
+  const path = 'https://jsonplaceholder.typicode.com/posts/1';
+
+  setUp(() async {
+    mockDioAdapter = MockDioAdapter();
+    mockDio = MockDio()..httpClientAdapter = mockDioAdapter;
+    httpServiceDioImpl = HttpServiceDioImpl(mockDio);
+  });
 
   group('get() | ', () {
     test(
       'SHOULD return a value WHEN [Dio] IS called',
       () async {
         // Arrange
-        when(() => mockDio.get<Map<String, dynamic>>(any(named: 'options')))
-            .thenAnswer(
-          (_) async => Response(
-            requestOptions: RequestOptions(),
-            data: mockData,
-            statusCode: 200,
-          ),
+        mockDioAdapter.onGet(
+          path,
+          (server) => server.reply(200, mockData),
         );
+
         // Act
-        final result = await httpServiceDioImpl.get<Map<String, dynamic>>('');
+        final result = await httpServiceDioImpl.get<Map<String, int>>(path);
         // Assert
         expect(result, equals(mockResponse));
         verify(() => mockDio.get<Map<String, dynamic>>(any()));
